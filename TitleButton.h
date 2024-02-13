5b05c26dd30649620502b210db65de40ff7bbe28
八基 後藤 <220385@jc-21.jp>
2024-02-13 11:34:25 +0900

    今タイトルボタンをボタン化してる不具合あれば元に戻してからメシ混むから今無理がんばって

--- TitleButton.h
+++ TitleButton.h
@@ -1,49 +1,15 @@
 #pragma once
-#include "Engine/GameObject.h"
-#include "Engine/Text.h"
+#include "Button.h"
 //タイトルのボタンを管理するクラス
-class TitleButton : public GameObject
+class TitleButton : public Button
 {
-    enum STATE {
-        IDLE,
-        SELECT,
-        PUSH,
-        SELECTED,
-        MAX
-    };
-    Text* buttonTextObj_;
-    std::string buttonName_;
-    int hImg_[MAX];
-    int scrW, scrH;
-    Text::HORIZONAL_ALIGNMENT hAl;
-    Text::VERTICAL_ALIGNMENT vAl;
-    int actHandle_;
-    XMINT4 clip = { 0,0,128,128 };
-    bool nextIdle = false;
 public:
-    STATE state;
     TitleButton(GameObject* parent);    //コンストラクタ
     ~TitleButton();                     //デストラクタ
     void Initialize() override; //初期化
     void Update() override;     //更新
     void Draw() override;       //描画
     void Release() override;    //解放
-    void SetText(std::string buttonName);
-    std::string LinkImageFile(STATE state);
-
-    std::string GetDebugStr(int i);
-
-    //自身(ボタン)と親の選択ステートを紐づける
-    void SetAction(int hAct);
-
-    //処理終了
-    void EndAct() { nextIdle = true; }
+    std::string LinkImageFile(STATE state) override;
 private:
-    void UpdateIdle();
-    void UpdateSelect();
-    void UpdatePush();
-    void UpdateSelected();
-
-    bool IsEntered();
-    bool Between(float value, float min, float max);
 };
