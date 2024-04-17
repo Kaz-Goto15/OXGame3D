530ace172334457b965bc6e2e21edb4586cdbc2a
八基 後藤 <220385@jc-21.jp>
2024-04-17 16:33:59 +0900

    難航Frame

--- Frame.cpp
+++ Frame.cpp
@@ -4,9 +4,11 @@
 #include "SystemConfig.h"
 
 //コンストラクタ
-Frame::Frame(GameObject* parent)
-    :GameObject(parent, "Frame"),
-    hFrameImg_(-1)
+Frame::Frame(GameObject* parent):
+    GameObject(parent, "Frame"),
+    hFrameImg_(-1),
+    fileName("Screen/frame256_2.png"),
+    edge(64)
 {
 }
 
@@ -18,7 +20,7 @@ Frame::~Frame()
 //初期化
 void Frame::Initialize()
 {
-     hFrameImg_ = Image::Load("Screen/frame256_2.png");
+     hFrameImg_ = Image::Load(fileName);
 }
 
 //更新
@@ -29,7 +31,13 @@ void Frame::Update()
 //描画
 void Frame::Draw()
 {
-    Image::Draw(hFrameImg_);
+    for (int h = FRAME_H::TOP; h < FRAME_H::MAX; h++) {
+        for (int w = FRAME_W::LEFT; w < FRAME_W::MAX; w++) {
+            Image::SetRect(hFrameImg_, w * edge, h * edge, edge, edge);
+            Image::SetTransform(hFrameImg_, tra[h][w]);
+            Image::Draw(hFrameImg_);
+        }
+    }
 }
 
 //開放
@@ -39,48 +47,47 @@ void Frame::Release()
 
 void Frame::TransformFrame(int up, int right, int down, int left)
 {
-    Image::SetRect(hFrameImg_, 0, 0, edge, edge);
-
+    //Image::SetRect(hFrameImg_, 0, 0, edge, edge);
+    const int DEFAULT_SCALE = 1;
+    const int DEFAULT_POSITION_X = 0;
+    const int DEFAULT_POSITION_Y = 0;
     using namespace SystemConfig;
     //frame構築
-    for (int y = 0; y < 3; y++) {
-        switch (y) {
-        case 0:
-            frameTra.position_.y = (float)(-screenHeight + (frameMargin.x + frameMargin.z + edge)) / 2.0f;
-            frameTra.scale_.y = 1;
-            break;
-        case 1:
-            frameTra.position_.y = 0;
-            frameTra.scale_.y = (float)(screenHeight - (frameMargin.x + frameMargin.z + edge + edge)) / (float)edge;
-            break;
-        case 2:
-            frameTra.position_.y = (float)(screenHeight - (frameMargin.x + frameMargin.z + edge)) / 2.0f;
-            frameTra.scale_.y = 1;
-            break;
-        }
-        for (int x = 0; x < 3; x++) {
-            switch (x) {
-            case 0:
-                frameTra.position_.x = (float)(-screenWidth + (frameMargin.y + frameMargin.w + edge)) / 2.0f;
-                frameTra.scale_.x = 1;
+    for (int y = FRAME_H::TOP; y < FRAME_H::MAX; y++) {
+        for (int x = FRAME_W::LEFT; x < FRAME_W::MAX; x++) {
+            switch (y) {
+            case FRAME_H::TOP:
+                tra[y][x].position_.y = Half(-screenHeight + (up + down + edge));
+                tra[y][x].scale_.y = DEFAULT_SCALE;
                 break;
-            case 1:
-                frameTra.position_.x = 0;
-                frameTra.scale_.x = (float)(screenWidth - (frameMargin.y + frameMargin.w + edge + edge)) / (float)edge;
+            case FRAME_H::CENTER:
+                tra[y][x].position_.y = DEFAULT_POSITION_Y;
+                tra[y][x].scale_.y = (float)(screenHeight - (up + down + edge + edge)) / (float)edge;
                 break;
-            case 2:
-                frameTra.position_.x = (float)(screenWidth - (frameMargin.y + frameMargin.w + edge)) / 2.0f;
-                frameTra.scale_.x = 1;
+            case FRAME_H::BOTTOM:
+                tra[y][x].position_.y = Half(screenHeight - (up + down + edge));
+                tra[y][x].scale_.y = DEFAULT_SCALE;
                 break;
             }
 
-            Image::SetRect(hPict_[PIC_BASIC_FRAME_TEX], x * edge, y * edge, edge, edge);
-            Transform tmp = frameTra;
+            switch (x) {
+            case FRAME_W::LEFT:
+                tra[y][x].position_.x = Half(-screenWidth + (right + left + edge));
+                tra[y][x].scale_.x = DEFAULT_SCALE;
+                break;
+            case FRAME_W::CENTER:
+                tra[y][x].position_.x = DEFAULT_POSITION_X;
+                tra[y][x].scale_.x = (float)(screenWidth - (right + left + edge + edge)) / (float)edge;
+                break;
+            case FRAME_W::RIGHT:
+                tra[y][x].position_.x = Half(screenWidth - (right + left + edge));
+                tra[y][x].scale_.x = DEFAULT_SCALE;
+                break;
+            }
 
-            tmp.SetReSize(frameScale);
+            //Image::SetRect(hFrameImg_, x * edge, y * edge, edge, edge);
 
-            Image::SetTransform(hPict_[p], tmp);
-            Image::Draw(hPict_[p]);
+            tra[y][x].SetReSize(transform_.scale_);
         }
     }
 }
--- Frame.h
+++ Frame.h
@@ -4,9 +4,28 @@
 //枠を管理するクラス
 class Frame : public GameObject
 {
-    const int FRAME_ここ何
+    enum FRAME_W {
+        LEFT= 0,
+        CENTER,
+        RIGHT,
+        MAX,
+    };
+    enum FRAME_H {
+        TOP = 0,
+        CENTER,
+        BOTTOM,
+        MAX
+    };
+    std::string fileName;
+    int edge;
     int hFrameImg_;
-    Transform tra[3][3];
+    Transform tra[FRAME_H::MAX][FRAME_W::MAX];
+
+    //上右下左でマージンを指定
+    void TransformFrame(XMINT4 margin) { TransformFrame(margin.x, margin.y, margin.z, margin.w); }
+    //上右下左でマージンを指定
+    void TransformFrame(int up, int right, int down, int left);
+
 public:
     Frame(GameObject* parent);    //コンストラクタ
     ~Frame();                     //デストラクタ
@@ -14,13 +33,9 @@ public:
     void Update() override;     //更新
     void Draw() override;       //描画
     void Release() override;    //解放
-    
-    //上右下左でマージンを指定
-    void TransformFrame(XMINT4 margin) { TransformFrame(margin.x, margin.y, margin.z, margin.w); }
-    //上右下左でマージンを指定
-    void TransformFrame(int up, int right, int down, int left);
 
+    void SetMargin(int up, int right, int down, int left)
 
     template <class T>
-    T Half(T* value) { return value / 2.0f };
+    float Half(T value) { return (float)value / 2.0f; }
 };
